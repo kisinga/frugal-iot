be4b9c527c73a62355b6c7ddc6234010e93a6e7e
kisinga <stevekisinga@gmail.com>
2026-05-18 00:35:28 +0300

    example: Actuator_Valve latching valve with platform files

---
+++ examples/valve/valve.ino
@@ -0,0 +1,24 @@
+/*
+ *  Frugal IoT example - Actuator_Valve latching valve
+ *
+ * Open/close coils on GPIO12 and GPIO14.
+ */
+
+#include "Frugal-IoT.h"
+
+System_Frugal frugal_iot("dev", "developers", "valve", "Latching Valve Demo");
+
+void setup() {
+  frugal_iot.configure_power(Power_Loop, 10000, 10000);
+  frugal_iot.pre_setup();
+  frugal_iot.configure_mqtt("frugaliot.naturalinnovation.org", "dev", "public");
+
+  frugal_iot.actuators->add(new Actuator_Valve("valve", "Valve", 12, 14, "blue"));
+
+  frugal_iot.setup();
+  Serial.println(F("FrugalIoT Valve Starting Loop"));
+}
+
+void loop() {
+  frugal_iot.loop();
+}
