967935d91034b771f5ec81ecea6ac2ce7cf020bf
kisinga <stevekisinga@gmail.com>
2026-05-18 00:33:43 +0300

    example: Sensor_Flow pulse counter with platform files

--- examples/flow/flow.ino
+++ examples/flow/flow.ino
@@ -1,5 +1,5 @@
 /*
- * Frugal IoT example - Sensor_Flow pulse counter
+ *  Frugal IoT example - Sensor_Flow pulse counter
  *
  * Connect a flow meter to a digital pin with pull-up.
  */
@@ -9,11 +9,10 @@
 System_Frugal frugal_iot("dev", "developers", "flow", "Flow Meter Demo");
 
 void setup() {
+  frugal_iot.configure_power(Power_Loop, 10000, 10000);
   frugal_iot.pre_setup();
   frugal_iot.configure_mqtt("frugaliot.naturalinnovation.org", "dev", "public");
-  frugal_iot.configure_power(Power_Loop, 10000, 10000);
 
-  // Flow sensor on GPIO4: 450 pulses/liter, fault below 0.5 L/min
   frugal_iot.sensors->add(new Sensor_Flow("flow", "Flow", 4, 450.0, 0.5, "blue", true));
 
   frugal_iot.setup();
