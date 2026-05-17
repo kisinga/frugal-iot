b437eb1c230665b9e60d76b2c8533040455bda89
kisinga <stevekisinga@gmail.com>
2026-05-18 00:33:43 +0300

    example: Control_Interlock pump guard with platform files

--- examples/interlock/interlock.ino
+++ examples/interlock/interlock.ino
@@ -1,5 +1,5 @@
 /*
- * Frugal IoT example - Control_Interlock
+ *  Frugal IoT example - Control_Interlock
  *
  * Turns on a pump only if flow confirms within 5 seconds.
  */
@@ -9,25 +9,18 @@
 System_Frugal frugal_iot("dev", "developers", "interlock", "Pump Interlock Demo");
 
 void setup() {
+  frugal_iot.configure_power(Power_Loop, 10000, 10000);
   frugal_iot.pre_setup();
   frugal_iot.configure_mqtt("frugaliot.naturalinnovation.org", "dev", "public");
-  frugal_iot.configure_power(Power_Loop, 10000, 10000);
 
-  // Flow sensor
-  Sensor_Flow* flow = new Sensor_Flow("flow", "Flow", 4, 450.0, 0.5, "blue", true);
-  frugal_iot.sensors->add(flow);
+  frugal_iot.sensors->add(new Sensor_Flow("flow", "Flow", 4, 450.0, 0.5, "blue", true));
 
-  // Pump relay on GPIO15
   Actuator_Digital* pump = new Actuator_Digital("pump", "Pump", 15, "blue");
   frugal_iot.actuators->add(pump);
 
-  // Interlock: command -> out -> pump; feedback from flow
   Control_Interlock* interlock = new Control_Interlock("interlock", "Interlock");
   frugal_iot.controls->add(interlock);
-
-  // Wire interlock output to pump
   interlock->outputs[0]->wireTo(frugal_iot.messages->path("pump/on"));
-  // Wire flow feedback to interlock
   interlock->inputs[1]->wireTo(frugal_iot.messages->path("flow/output"));
 
   frugal_iot.setup();
