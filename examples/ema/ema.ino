375d92b08f56376f58fe9c5ca903bc6e0be600c5
kisinga <stevekisinga@gmail.com>
2026-05-18 00:33:43 +0300

    example: Control_EMA filter with platform files

--- examples/ema/ema.ino
+++ examples/ema/ema.ino
@@ -1,7 +1,8 @@
 /*
- * Frugal IoT example - Control_EMA filter
+ *  Frugal IoT example - Control_EMA filter
  *
  * Smooths noisy sensor data with an exponential moving average.
+ * Wire any Sensor_Float output to the EMA's raw input.
  */
 
 #include "Frugal-IoT.h"
@@ -9,19 +10,15 @@
 System_Frugal frugal_iot("dev", "developers", "ema", "EMA Filter Demo");
 
 void setup() {
+  frugal_iot.configure_power(Power_Loop, 10000, 10000);
   frugal_iot.pre_setup();
   frugal_iot.configure_mqtt("frugaliot.naturalinnovation.org", "dev", "public");
-  frugal_iot.configure_power(Power_Loop, 10000, 10000);
 
-  // Add a sensor (e.g., analog)
   Sensor_Analog* sensor = new Sensor_Analog("analog", "Analog", A0, "blue", true);
   frugal_iot.sensors->add(sensor);
 
-  // Add EMA filter
   Control_EMA* ema = new Control_EMA("ema", "EMA", 0.0, 2, 0.0, 1023.0);
   frugal_iot.controls->add(ema);
-
-  // Wire sensor -> EMA
   ema->inputs[0]->wireTo(frugal_iot.messages->path("analog/output"));
 
   frugal_iot.setup();
